d3ac9b708ab176bd62af231a301bffe376035073
strongcode9527 <1477869317@qq.com>
2018-05-09 23:14:48 +0800

    :sparkles:

--- App.js
+++ App.js
@@ -1,8 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
@@ -19,8 +14,8 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
